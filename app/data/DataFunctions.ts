ff88db9d1fc8219cc0c81c1abbb674785cbfafd8
grafica <paolo@monema.it>
2022-12-07 12:11:03 +0100

    problema build su netlify

--- app/data/DataFunctions.ts
+++ app/data/DataFunctions.ts
@@ -2,20 +2,21 @@ import fs from "fs/promises";
 
 
 export async function getUsersData() {
-  const rawMockUsers = await fs.readFile("./mock.json", "utf-8");
+  const rawMockUsers = await fs.readFile("~/data/mock.json", "utf-8");
   const jMockUsers = JSON.parse(rawMockUsers);
   const MockUsers = jMockUsers.users ?? [];
   return MockUsers;
 }
 
 export async function getProductsData() {
-  const rawProduct = await fs.readFile("./products.json", "utf-8");
+  const rawProduct = await fs.readFile("~/data/products.json", "utf-8");
   const jProduct = JSON.parse(rawProduct);
   const product = jProduct ?? [];
   return product;
 }
+
 export async function getValidazioni() {
-  const rawValidazioni = await fs.readFile("./validazioni.json", "utf-8");
+  const rawValidazioni = await fs.readFile("~/data/validazioni.json", "utf-8");
   const jValidazioni = JSON.parse(rawValidazioni);
   const validazioni = jValidazioni ?? [];
   return validazioni;
