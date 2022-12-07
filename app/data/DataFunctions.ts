ae3f710dd53ff5b13b1abe19fc08f58b9b8da1a4
grafica <paolo@monema.it>
2022-12-07 12:26:15 +0100

    problema build su netlify

--- app/data/DataFunctions.ts
+++ app/data/DataFunctions.ts
@@ -2,30 +2,29 @@ import fs from "fs/promises";
 
 
 export async function getUsersData() {
-  const rawMockUsers = await fs.readFile("~/data/mock.json", "utf-8");
+  const rawMockUsers = await fs.readFile("./app/data/mock.json", "utf-8");
   const jMockUsers = JSON.parse(rawMockUsers);
   const MockUsers = jMockUsers.users ?? [];
   return MockUsers;
 }
 
 export async function getProductsData() {
-  const rawProduct = await fs.readFile("~/data/products.json", "utf-8");
+  const rawProduct = await fs.readFile("./app/data/products.json", "utf-8");
   const jProduct = JSON.parse(rawProduct);
   const product = jProduct ?? [];
   return product;
 }
-
 export async function getValidazioni() {
-  const rawValidazioni = await fs.readFile("~/data/validazioni.json", "utf-8");
+  const rawValidazioni = await fs.readFile("./app/data/validazioni.json", "utf-8");
   const jValidazioni = JSON.parse(rawValidazioni);
   const validazioni = jValidazioni ?? [];
   return validazioni;
 }
 
 export function writeProductsData(products: []) {
-  return fs.writeFile('./products.json', JSON.stringify({ products: products || [] }))
+  return fs.writeFile('./app/data/products.json', JSON.stringify({ products: products || [] }))
 }
 export function writeUserData(users: []) {
-  return fs.writeFile('./mock.json', JSON.stringify({ users: users || [] }))
+  return fs.writeFile('./app/data/mock.json', JSON.stringify({ users: users || [] }))
 }
 
