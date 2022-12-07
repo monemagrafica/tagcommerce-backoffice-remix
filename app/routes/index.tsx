6942e9ea778963b0358a1b5d76d335065a151119
grafica <paolo@monema.it>
2022-12-07 12:57:12 +0100

    problema build su netlify

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,23 +1,23 @@
-import { json } from "@remix-run/node";
-import { useCatch, useLoaderData } from "@remix-run/react";
-import { getProductsData } from "../data/DataFunctions";
+import { json } from "@remix-run/node"
+import { useCatch, useLoaderData } from "@remix-run/react"
+import { getProductsData } from "../data/DataFunctions"
 
 export default function Index() {
-  const mock = useLoaderData();
-  console.log(mock);
+  const mock = useLoaderData()
+  console.log(mock)
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
       <h1>Welcome to Remix</h1>
       <ul>
         {mock.products.map((item: { id: number; name: string }) => {
-          return <li key={item.id}>{item.name}</li>;
+          return <li key={item.id}>{item.name}</li>
         })}
       </ul>
     </div>
-  );
+  )
 }
 export async function loader() {
-  const products = await getProductsData();
+  const products = await getProductsData()
 
   if (!products || products.length === 0) {
     throw json(
@@ -26,12 +26,23 @@ export async function loader() {
         status: 404,
         statusText: "Prodotti non trovati",
       }
-    );
+    )
   }
-  return products;
+  return {
+    products: [
+      {
+        id: 1,
+        name: "test",
+      },
+      {
+        id: 2,
+        name: "test2",
+      },
+    ],
+  }
 }
 export function CatchBoundary() {
-  const getError = useCatch();
-  const message = getError.data.message || "data not found";
-  return <main>{message}</main>;
+  const getError = useCatch()
+  const message = getError.data.message || "data not found"
+  return <main>{message}</main>
 }
