cbdca0b18b088fe49a19e6e1ca6ab47d890a07e4
grafica <paolo@monema.it>
2022-12-09 11:16:36 +0100

    controlli su form login

--- app/components/login/forms.tsx
+++ app/components/login/forms.tsx
@@ -3,26 +3,24 @@ import { withZod } from "@remix-validated-form/with-zod";
 import { z } from "zod";
 import { InputForm } from "../inputForm/inputForm";
 import { SubmitButton } from "../inputForm/submitButton";
+import messaggiValidazione from "../../data/validazioni.json";
 
 const validator = withZod(
   z.object({
-    firstName: z.string().min(1, { message: "First name is required" }),
-    lastName: z.string().min(1, { message: "Last name is required" }),
     email: z
       .string()
-      .min(1, { message: "Email is required" })
-      .email("Must be a valid email"),
+      .min(8, { message: messaggiValidazione.emailLength })
+      .email(messaggiValidazione.email),
+    password: z.string().min(8, { message: messaggiValidazione.password }),
   })
 );
 
 function FormLogin() {
-  console.log("dio merda");
-
   return (
     <ValidatedForm validator={validator} method="post">
-      <InputForm name="firstName" label="First Name" type="text" />
-      <InputForm name="lastName" label="Last Name" type="text" />
       <InputForm name="email" label="Email" type="email" />
+      <InputForm name="password" label="Password" type="password" />
+
       <SubmitButton />
     </ValidatedForm>
   );
--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -1,63 +1,10 @@
-import type { ActionFunction } from "@remix-run/node";
-import type { FC } from "react";
-
-import { redirect } from "@remix-run/node";
 import { FormLogin } from "~/components/login/forms";
-import { getUsersData, getValidazioni } from "~/data/DataFunctions";
-import {
-  validazioneCampoMail,
-  validazioneCampoPassword,
-} from "../data/FormValidationFunctions";
-import { Link } from "@remix-run/react";
 
-const Login: FC = () => {
+const Login = () => {
   return (
     <div className="wrapperLogin">
       <FormLogin />
     </div>
   );
 };
-
-export const action: ActionFunction = async ({ request }) => {
-  const formData = await request.formData();
-  const dataUser = Object.fromEntries(formData);
-  const datiEsistenti = await getUsersData();
-  const validazioni = await getValidazioni();
-
-  console.log("formdata", datiEsistenti);
-  console.log("formdata", dataUser);
-  //controllo campi
-  if (!validazioneCampoMail(dataUser.loginEmail)) {
-    return { message: validazioni.email };
-  }
-  if (!validazioneCampoPassword(dataUser.loginPassword)) {
-    return { message: validazioni.password };
-  }
-  if (
-    //controlla se l'utente è gia inserito
-    datiEsistenti.find((item: { mail: string; password: string }) => {
-      return (
-        dataUser.loginEmail === item.mail &&
-        dataUser.loginPassword === item.password
-      );
-    })
-  ) {
-    return redirect("/");
-  } else {
-    console.log("errore di autenticazione");
-    return null;
-  }
-};
 export default Login;
-
-export function ErrorBoundary({ error }: { error: Error }) {
-  return (
-    <>
-      <h1>Si è verificato un errore</h1>
-      <p>{error.message}</p>
-      <div>
-        torna alla <Link to="/">Home</Link>
-      </div>
-    </>
-  );
-}
