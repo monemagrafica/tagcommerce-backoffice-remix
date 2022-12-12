5d27873c9b4ed265b27ebf7d0e609456e6eb7bec
grafica <paolo@monema.it>
2022-12-12 18:22:10 +0100

    test animazione modale

--- app/components/prodotti/modaleProdotto.tsx
+++ app/components/prodotti/modaleProdotto.tsx
@@ -2,6 +2,7 @@ import { Link } from "@remix-run/react";
 import { type FC } from "react";
 import type { prodotto } from "~/types/prodotti";
 import { FormProdotto } from "./formsProdotti";
+import { AnimatePresence, motion } from "framer-motion";
 
 type Validazioni = {
   nome: string;
@@ -18,10 +19,18 @@ const ModaleProdotto: FC<Props> = ({ prodotto, validazioni }) => {
       <Link to="..">
         <div className="backgroundModale"></div>
       </Link>
-      <div className="wrapperModaleProdotto">
-        {prodotto.name}
-        <FormProdotto prodotto={prodotto} validazioneForm={validazioni} />
-      </div>
+      <AnimatePresence>
+        <motion.div
+          className="wrapperModaleProdotto"
+          initial={{ opacity: 0, y: 100 }}
+          exit={{ opacity: 0, y: 100 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.3 }}
+        >
+          {prodotto.name}
+          <FormProdotto prodotto={prodotto} validazioneForm={validazioni} />
+        </motion.div>
+      </AnimatePresence>
     </>
   );
 };
